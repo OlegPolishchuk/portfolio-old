82d6c937a92881d92b9e5d246eed8ac78279d14c
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-04-24 22:58:15 +0300

    fix: header container

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import s from './Header.module.css'
+import sContainer from '../../common/styles/Container.module.css'
 import {Nav} from "../Nav/Nav";
 
 export const Header = () => {
 
     return (
         <header className={s.header}>
-            <Nav />
+            <div className={`${sContainer.container} ${s.container}`}>
+                <Nav/>
+            </div>
         </header>
     )
 }
