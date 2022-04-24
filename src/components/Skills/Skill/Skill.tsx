5c04fee42f303a1ef8b0318c0e137545312d4678
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-04-24 22:40:08 +0300

    feat: add Skill item structure

---
+++ src/components/Skills/Skill/Skill.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import s from './Skill.module.css'
+
+type SkillPropsType = {
+    title: string
+    description: string
+}
+
+export const Skill: React.FC<SkillPropsType> = (props) => {
+    return (
+        <div className={s.skillBlock}>
+            <div className={s.icon}>
+            </div>
+            <h3>{props.title}</h3>
+            <span className={s.description}>{props.description}</span>
+        </div>
+    )
+}
