45562af87820ba408c14c4402cdfde1d23032371
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-04-24 21:39:54 +0300

    feat: add common styles (Container.module.css)

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import s from './Main.module.css'
+import sContainer from '../../common/styles/Container.module.css'
 
 export const Main = () => {
     return (
         <section className={s.mainBlock}>
-           <div className={`container ${s.mainContainer}`}>
+           <div className={`${sContainer.container} ${s.mainContainer}`}>
                <div className={s.title}>
                    <span>Hi there</span>
                    <h1>I am Jeffrey Aaron</h1>
