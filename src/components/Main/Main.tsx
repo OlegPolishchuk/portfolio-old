1782c5b7c795624639c6c7b1fa6d421a547b3e63
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-04-24 21:07:28 +0300

    feat: add Main block structure

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import s from './Main.module.css'
+
+export const Main = () => {
+    return (
+        <section className={s.mainBlock}>
+           <div className={`container ${s.mainContainer}`}>
+               <div className={s.title}>
+                   <span>Hi there</span>
+                   <h1>I am Jeffrey Aaron</h1>
+                   <p>Frontend Developer</p>
+               </div>
+               <div className={s.photo}>
+
+               </div>
+           </div>
+        </section>
+    )
+}
