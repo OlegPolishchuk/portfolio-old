369cbe60fcbce865c8c055e555abae026221a0e2
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-05-09 17:45:36 +0300

    feat: add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import sContainer from '../../common/styles/Container.module.css';
+import s from './Footer.module.css';
+import Social from "./Social/Social";
+
+const Footer = () => {
+    return (
+        <footer className={s.footer}>
+            <div className={`${sContainer.container} ${s.container}`}>
+                <h2 className={s.title}>Oleg Polishchuk</h2>
+                <div className={s.socialsBlock}>
+                    <Social/>
+                    <Social/>
+                    <Social/>
+                    <Social/>
+                </div>
+                <span className={s.copyright}>2022</span>
+            </div>
+
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Footer/Social/Social.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Social.module.css'
+
+const Social = () => {
+    return (
+        <div className={s.social}>
+
+        </div>
+    );
+};
+
+export default Social;
