7b8a29e90320c5a183e2752afb3e7008b8a694d7
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-05-29 11:31:11 +0300

    feat: add background image to Project component by style

--- src/components/Projects/Project/Project.tsx
+++ src/components/Projects/Project/Project.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import s from './Project.module.css'
 
-const Project = () => {
+type PropsType = {
+    title: string,
+    description: string,
+    style?: React.CSSProperties
+}
+
+const Project: React.FC<PropsType> = ({title, description, style}) => {
+
+    const finalStyle =  style ? style : {}
+
     return (
         <div className={s.projectContainer}>
-            <div className={s.imgContainer}>
+            <div className={s.imgContainer} style={finalStyle}>
                 <a href={''} className={s.projectLink}>Project</a>
             </div>
             <div className={s.projectDescription}>
-                <h3 className={s.title}>Project name</h3>
-                <span className={s.description}>description</span>
+                <h3 className={s.title}>{title}</h3>
+                <span className={s.description}>{description}</span>
             </div>
         </div>
     );
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import sContainer from '../../common/styles/Container.module.css'
-import s from './Projects.module.css'
-import Project from "./Project/Project";
-import Title from "../../common/components/title/Title";
+import sContainer from '../../common/styles/Container.module.css';
+import s from './Projects.module.css';
+import Project from './Project/Project';
+import Title from '../../common/components/title/Title';
+import social from './../../assets/image/social-network-background-with-icons_23-2147497535.jpg'
+import todo from './../../assets/image/todolist.jpg'
+
 
 const Projects = () => {
+
+    const socialNW = {
+        backgroundImage: `url(${social})`,
+    }
+
+    const todoList = {
+        backgroundImage: `url(${todo})`
+    }
+
     return (
         <section className={s.projectsBlock}>
             <div className={`${sContainer.container} ${s.projects}`}>
@@ -13,8 +25,8 @@ const Projects = () => {
                 {/*</div>*/}
                 <Title title={'Projects'}/>
                 <div className={s.projectsContainer}>
-                    <Project/>
-                    <Project/>
+                    <Project title={'Social Network'} description={'some description'} style={socialNW}/>
+                    <Project title={'Todo list'} description={'some description'} style={todoList}/>
                 </div>
             </div>
         </section>
