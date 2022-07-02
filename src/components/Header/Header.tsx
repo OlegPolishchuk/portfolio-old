bb9a5f4fd25f3e853123712b46c3aa2a3d1bc53f
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-07-02 14:34:56 +0300

    feat: add aside with nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import s from './Header.module.css'
 import sContainer from '../../common/styles/Container.module.css'
 import {Nav} from "../Nav/Nav";
+import {Main} from "../Main/Main";
 
 export const Header = () => {
 
-    return (
-        <header className={s.header}>
-            <div className={`${sContainer.container} ${s.container}`}>
-                <Nav/>
-            </div>
-        </header>
-    )
+  return (
+    <header className={s.header}>
+      {/*<div className={`${sContainer.container} ${s.container}`}>*/}
+      {/*  <Nav/>*/}
+      {/*  <Main/>*/}
+      {/*</div>*/}
+      <Nav/>
+      <Main/>
+    </header>
+  )
 }
