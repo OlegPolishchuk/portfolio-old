6df51e9be1bc678d37ca6fb3b116b28661a274d9
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-07-12 21:59:45 +0300

    fix: change structure of Contacts component

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import s from './Contacts.module.css';
+import s from './Contacts.module.scss';
 import sContainer from '../../common/styles/Container.module.css';
 import Form from "../Form/Form";
+import Title from "../../common/components/title/Title";
 
 const Contacts = () => {
-    return (
-        <section className={s.contactsBlock}>
-            <div className={`${sContainer.container} ${s.contacts}`}>
-                <h2 className={s.title}>Contacts</h2>
-                <Form/>
-                <a href={''} className={s.btnForm}>Send</a>
-            </div>
-        </section>
-    );
+  return (
+    <section className={s.contactsBlock}>
+      <div className={`${sContainer.container} ${s.contacts}`}>
+        <Title title={'Contacts'}/>
+        <Form/>
+        <a href={''} className={s.btnForm}>Send</a>
+      </div>
+    </section>
+  );
 };
 
 export default Contacts;
