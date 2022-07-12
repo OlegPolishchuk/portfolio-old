3a553eb5faba0761612e20c9d379d52be54841e4
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-07-12 22:19:08 +0300

    fix: add margin to skill block

--- src/components/Skills/Skill/Skill.tsx
+++ src/components/Skills/Skill/Skill.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import s from './Skill.module.css'
+import s from './Skill.module.scss'
 
 type SkillPropsType = {
     title: string
@@ -11,7 +11,7 @@ export const Skill: React.FC<SkillPropsType> = (props) => {
         <div className={s.skillBlock}>
             <div className={s.icon}>
             </div>
-            <h3>{props.title}</h3>
+            <h3 className={s.skillTitle}>{props.title}</h3>
             <span className={s.description}>{props.description}</span>
         </div>
     )
