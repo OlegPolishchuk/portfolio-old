6e0238288098d304e13a829de2ce8be103bef101
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-07-17 15:28:44 +0300

    fix: edit nav styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import s from './Header.module.scss'
 import {Nav} from "../Nav/Nav";
 import {Main} from "../Main/Main";
@@ -7,8 +7,17 @@ import {ToggleBtn} from "../../common/components/ToggleBtn/ToggleBtn";
 
 export const Header = () => {
 
+  const [visible, setVisible] = useState(false)
+
   const handleToggle = () => {
+    setVisible(!visible)
     console.log('toggle')
+  }
+
+  const topValue = visible ? '70px' : '-200px';
+
+  const style = {
+    top: `${topValue}`
   }
 
   return (
@@ -16,7 +25,7 @@ export const Header = () => {
       <aside className={s.aside}>
         <Photo/>
         <ToggleBtn toggle={handleToggle} />
-        <Nav/>
+        <Nav style={style}/>
       </aside>
       <Main/>
     </header>
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import s from './Nav.module.scss'
 
-export const Nav = () => {
+type PropsType = {
+  style: React.CSSProperties
+}
+
+export const Nav = ({style}: PropsType) => {
+
     return (
-        <nav className={s.nav}>
+        <nav className={s.nav} style={style}>
             <a href="#">Main</a>
             <a href="#">Skills</a>
             <a href="#">Projects</a>
