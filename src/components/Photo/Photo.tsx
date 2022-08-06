8dc4bdda33d268794fcc9536dc79f054c12b150b
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-08-06 21:47:06 +0300

    feat: change btn styles

--- src/components/Photo/Photo.tsx
+++ src/components/Photo/Photo.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import s from './Photo.module.scss';
-import photo from '../../assets/image/log.jpg'
+import photo from '../../assets/image/log.jpg';
+import Tilt from 'react-parallax-tilt';
 
 const Photo = () => {
 
-  const photoStyle = {
-    backgroundImage: `url(${photo})`
-  }
+    const photoStyle = {
+        backgroundImage: `url(${photo})`
+    }
 
-  return (
-    <div className={s.photoContainer}>
-      <a href={'#'}>
-        <div className={s.photo} style={photoStyle}/>
-        <h2 className={s.title}>Polishchuk Oleg</h2>
-      </a>
-    </div>
-  );
+    return (
+        <div className={s.photoContainer}>
+            <a href={'#'}>
+                <Tilt
+                    glareEnable={true}
+                    glareMaxOpacity={0.8}
+                    glareColor="#ffffff"
+                    glarePosition="bottom"
+                    glareBorderRadius="20px"
+                >
+                    <div className={s.photo} style={photoStyle}/>
+                </Tilt>
+                <h2 className={s.title}>Polishchuk Oleg</h2>
+            </a>
+        </div>
+    );
 };
 
 export default Photo;
